bcc048020beb332ef16b82ed29fb151a8d2b4e78
James Cho <hwisoocho@gmail.com>
2018-04-22 21:31:45 -0700

    forward and back, bgcolors

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 // 1.Date and Time Function
 
  function dayAndTime() {
-var today = new Date();
-var day = today.getDay();
-var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-var displayDay = ("Today is : " + daylist[day] + ".");
-var hour = today.getHours();
-var minute = today.getMinutes();
-var second = today.getSeconds();
-var prepand = (hour >= 12)? " PM ":" AM ";
-hour = (hour >= 12)? hour - 12: hour;
-if (hour===0 && prepand===' PM ') 
-{ 
-if (minute===0 && second===0)
-{ 
-hour=12;
-prepand=' Noon';
-} 
-else
-{ 
-hour=12;
-prepand=' PM';
-} 
-} 
-if (hour===0 && prepand===' AM ') 
-{ 
-if (minute===0 && second===0)
-{ 
-hour=12;
-prepand=' Midnight';
-} 
-else
-{ 
-hour=12;
-prepand=' AM';
-} 
-} 
-var displayTime = ("Current Time : "+hour + prepand + " : " + minute + " : " + second);
+    var today = new Date();
+    var day = today.getDay();
+    var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+    var displayDay = ("Today is : " + daylist[day] + ".");
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var second = today.getSeconds();
+    var prepand = (hour >= 12)? " PM ":" AM ";
+    hour = (hour >= 12)? hour - 12: hour;
+    if (hour===0 && prepand===' PM ') 
+    { 
+    if (minute===0 && second===0)
+    { 
+    hour=12;
+    prepand=' Noon';
+    } 
+    else
+    { 
+    hour=12;
+    prepand=' PM';
+    } 
+    } 
+    if (hour===0 && prepand===' AM ') 
+    { 
+    if (minute===0 && second===0)
+    { 
+    hour=12;
+    prepand=' Midnight';
+    } 
+    else
+    { 
+    hour=12;
+    prepand=' AM';
+    } 
+    } 
+    var displayTime = ("Current Time : "+hour + prepand + " : " + minute + " : " + second);
 
-document.getElementById("timer").innerHTML = displayDay+' '+ displayTime;
+    document.getElementById("timer").innerHTML = displayDay+' '+ displayTime;
 
 }
-// End of 1. Date and Time Function
+
 
 // 2. Formatted Date
 
@@ -70,14 +70,16 @@ today = dd+'/'+mm+'/'+yyyy;
 document.getElementById("date4").innerHTML =(today);
 }
 
-//End of 2. Formatted Date
+
 
 // 3.Print the current page
+
 function print_current_page()
 {
 window.print();
 }
-// End of print the current page 
+
+
 
 // 4. Random Number Game
 
@@ -93,47 +95,27 @@ function randomNumber(){
 
 } 
 
-// End of Random Number Game
 
-// 5. Multiplication Table
 
-        
-var rows = prompt("How many rows for your multiplication table?");
-var cols = prompt("How many columns for your multiplication table?");
-if(rows == "" || rows == null)
-        rows = 10;
-if(cols== "" || cols== null)
-        cols = 10;
-createTable(rows, cols);
-function createTable(rows, cols)
-{
-  var j=1;
-  var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
-  for(i=1;i<=rows;i++)
-  {
-    output = output + "<tr>";
-    while(j<=cols)
-    {
-        output = output + "<td>" + i*j + "</td>";
-         j = j+1;
-       }
-        output = output + "</tr>";
-        j = 1;
-}
-output = output + "</table>";
+// 5. Forward and Back Buttons
 
-document.write(output);
+function goBack() {
+	window.history.back();
 }
 
-    // End Multiplication Table
+function goForward() {
+	window.history.back();
+}
 
-    // 6. Hover message
+    
 
-    function trigger()
+// 6. Hover message
+
+function trigger()
 
 	{
 
-	document.getElementById("hover").addEventListener("mouseover", popup);
+	document.getElementById("hover").addEventListener("mouseover", popup());
 
 	function popup()
 
@@ -145,4 +127,14 @@ document.write(output);
 
     }
     
-    // End Hover message
+    
+// 7. Change Background color
+    function changeColor(){
+
+        document.getElementById("bgColor").style.backgroundColor = "lightblue";
+
+    }
+
+    function changeBack(){
+        document.getElementById("bgColor").style.backgroundColor = "white";
+    }
